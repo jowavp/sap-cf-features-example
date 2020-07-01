6f415242a537920fa361f95e05d48cc59c655dce
Joachim <joachim.vanpraet@flexso.com>
2020-07-01 18:42:52 +0200

    identifier function as parameter

--- approuter/approuter.js
+++ approuter/approuter.js
@@ -1,8 +1,9 @@
 const approuter = require("@sap/approuter")();
 const {featureFlagRouter} = require("sap-cf-features");
 
+// req.user.tenant contains the name of the tenant.
 approuter.beforeRequestHandler.use(
-  '/feature-flags', featureFlagRouter(true)
+  '/feature-flags', featureFlagRouter( (req) => req.user.tenant, true)
 );
 
 approuter.start();
